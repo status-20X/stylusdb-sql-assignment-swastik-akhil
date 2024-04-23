cf4aedb84885fad58d4450cb35924c87419aae54
Akhil Agrawal <akhilmindset@gmail.com>
2024-04-23 17:13:14 +0530

    solved intial assignments

---
+++ tests/index.test.js
@@ -0,0 +1,12 @@
+// tests/index.test.js
+
+const parseQuery = require('../src/queryParser');
+
+test('Parse SQL Query', () => {
+    const query = 'SELECT id, name FROM sample';
+    const parsed = parseQuery(query);
+    expect(parsed).toEqual({
+        fields: ['id', 'name'],
+        table: 'sample'
+    });
+});
